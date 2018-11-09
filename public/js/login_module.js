5cb374daa2e9b7fe59619eedbc9b668c7b1cbd67
shubhamkr17 <sk767285@gmail.com>
2018-11-09 22:59:10 +0530

    update user profile + complete order circle

--- public/js/login_module.js
+++ public/js/login_module.js
@@ -1,19 +1,23 @@
 var isRetailer = true;
 var isDoctor = true;
 function patient_login () {
-    if(isLoggedIn())
+    if(gid=isLoggedIn())
     {
-        sessionStorage.setItem('gid','123456');
+        sessionStorage.setItem('patient_gid',gid);
         window.location.href="profile_patient.html";
     }
     else
         login();
 }
 function retailer_login () {
-    if(isLoggedIn())
+    if(gid=isLoggedIn())
     {
+        
         if(isRetailer==true)
+        {
+            sessionStorage.setItem('retailer_gid',gid);
             window.location.href="profile_retailer.html";
+        }
         else
         window.location.href="become_retailer.html";
     }
@@ -21,10 +25,13 @@ function retailer_login () {
         login();
 }
 function doctor_login () {
-    if(isLoggedIn())
+    if(isLoggedIn(gid))
     {
         if(isDoctor==true)
+        {
+            sessionStorage.setItem('doctor_gid',gid);
             window.location.href="profile_doctor.html";
+        }
         else
         window.location.href="become_doctor.html";
     }
@@ -35,8 +42,11 @@ function doctor_login () {
 
 
 function isLoggedIn() {
-    sessionStorage.setItem('gid', '123456');
-    return true;
+    //sessionStorage.setItem('gid', '12345');
+    //gid from google 
+    var gid = "12345";
+    sessionStorage.setItem('user_image_url',"/files/prescriptions/1/index.jpeg");
+    return gid;
 }
 
 
