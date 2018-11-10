00c98feadd7755243b049a3068c163a70bd8336a
shubhamkr17 <sk767285@gmail.com>
2018-11-11 03:24:08 +0530

    added time slab decoder

---
+++ public/js/enc_dec_time.js
@@ -0,0 +1,36 @@
+var str = "1420004";
+var day = ['','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
+var span = ['','Morning','Afternoon','Evening','Night'];
+var slot = "";
+function get_time(str)
+{
+    slot+=day[parseInt(str[0])] + ", ";
+    slot+=span[parseInt(str[1])]+ ", ";
+
+    if(parseInt(str[1])==1)
+    {
+        slot+= parseInt(str[2])+5+":00 AM,";
+    }
+    else
+    if(parseInt(str[1])==3)
+    {
+        slot+= parseInt(str[2])+5+":00 PM,";
+    }
+    else
+    if(parseInt(str[1])==2)
+    {
+        slot+= parseInt(str[2])-1+":00 PM,";
+    }
+    else
+    if(parseInt(str[1])==4)
+    {
+        slot+= parseInt(str[2])-1+":00 AM,";
+    }
+
+
+    slot+=" S.NO : "+parseInt(str[5]+str[6]);
+
+    console.log("slot",slot);
+}
+
+get_time(str);
